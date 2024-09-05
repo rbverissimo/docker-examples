47141cc4b7fae40fab86991375529c4e4a78f6e9
Renato <rverissimo.dev@gmail.com>
2024-09-05 09:43:16 -0300

    Making it to possible to work two applications in a single amazon machine

--- node-example-deploy/app.js
+++ node-example-deploy/app.js
@@ -6,9 +6,9 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+app.get('/ned', (req, res) => {
   const filePath = path.join(__dirname, 'views', 'welcome.html');
   res.sendFile(filePath);
 });
 
-app.listen(80);
+app.listen(89);
